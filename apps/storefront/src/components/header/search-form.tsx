ec6d36d89823caa56536734bea78c18df2aef260
soniaklimas <57659625+soniaklimas@users.noreply.github.com>
2024-10-03 14:28:41 +0200

    fix: Close search form after submitting (#23)
    * fix: Improve search experience (#18)
    
    * fix: Improve search experience
    
    * Search on icon click
    
    * Add margin top to header when items in bag
    
    * Use cn
    
    * fix: Close search form after submitting
    
    * Make onSubmit optional

--- apps/storefront/src/components/header/search-form.tsx
+++ apps/storefront/src/components/header/search-form.tsx
@@ -54,7 +54,7 @@ const initialSearchState: SearchState = {
   showOptions: false,
 };
 
-export const SearchForm = () => {
+export const SearchForm = ({ onSubmit }: { onSubmit?: () => void }) => {
   const ts = useTranslations("search");
   const tc = useTranslations("common");
 
@@ -85,6 +85,9 @@ export const SearchForm = () => {
     if (event.code === keyboardCodes.Enter) {
       event.preventDefault();
       resetSearchState();
+      if (onSubmit) {
+        onSubmit();
+      }
 
       // Handle query search
       if (isNoOptionHighlighted || isLastOptionHighlighted) {
@@ -130,6 +133,7 @@ export const SearchForm = () => {
   const handleSearchIconClick = () => {
     if (isNoOptionHighlighted || isLastOptionHighlighted) {
       router.push(paths.search.asPath({ query: { q: inputValue } }));
+      resetSearchState();
 
       return;
     }
