9f82945140192789412084fda834dd525a5a5d58
soniaklimas <57659625+soniaklimas@users.noreply.github.com>
2024-10-25 11:18:25 +0200

    feat: Improve seo (#57)
    * feat: Improve seo
    
    * Ajust header tags
    
    * Implement review
    
    * Revert h1 tag to h2

--- apps/storefront/src/app/robots.ts
+++ apps/storefront/src/app/robots.ts
@@ -2,9 +2,13 @@ import { type MetadataRoute } from "next";
 
 export default function robots(): MetadataRoute.Robots {
   return {
-    rules: {
-      userAgent: "*",
-      allow: "/",
-    },
+    rules: [
+      {
+        userAgent: "*",
+        allow: "/",
+        disallow: ["/api/", "/_next/", "/_vercel/"],
+      },
+    ],
+    sitemap: `${process.env.BASE_URL}/sitemap.xml`,
   };
 }
