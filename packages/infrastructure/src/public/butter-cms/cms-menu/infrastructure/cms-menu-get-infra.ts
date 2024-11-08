c9b618da0e5d36c8eaa59189a9be3890d4b50dda
soniaklimas <57659625+soniaklimas@users.noreply.github.com>
2024-11-08 13:10:14 +0100

    Fix locale issue (#69)

--- packages/infrastructure/src/public/butter-cms/cms-menu/infrastructure/cms-menu-get-infra.ts
+++ packages/infrastructure/src/public/butter-cms/cms-menu/infrastructure/cms-menu-get-infra.ts
@@ -11,13 +11,21 @@ import type { ButterCMSMenuServiceConfig } from "../types";
 export const butterCMSMenuGetInfra =
   ({ token }: ButterCMSMenuServiceConfig): CMSMenuGetInfra =>
   async ({ languageCode, slug }) => {
-    const locale = languageCode ? convertLanguageCode(languageCode) : undefined;
+    const locale = convertLanguageCode(languageCode);
+    let menu;
 
-    // @ts-expect-error due to deep type inference issues with ButterCMS types
-    const menu = await Butter(token).content.retrieve(
-      ["navigation_menu"],
-      locale ? { locale } : undefined,
-    );
+    try {
+      // @ts-expect-error due to deep type inference issues with ButterCMS types
+      menu = await Butter(token).content.retrieve(
+        ["navigation_menu"],
+        locale ? { locale } : undefined,
+      );
+    } catch (error) {
+      // Fallback to 'EN_US' if the initial request fails
+      menu = await Butter(token).content.retrieve(["navigation_menu"], {
+        locale: "enus",
+      });
+    }
 
     if (!menu?.data?.data?.navigation_menu) {
       return null;
