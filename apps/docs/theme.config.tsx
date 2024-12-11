bf240a8d67ad269a86f61045c0697c831ecdb926
Karolka <13152501+karolkarolka@users.noreply.github.com>
2024-12-11 14:01:06 +0100

    feat: Bump nextra to v3 (#108)

--- apps/docs/theme.config.tsx
+++ apps/docs/theme.config.tsx
@@ -1,27 +1,34 @@
-import type { DocsThemeConfig } from "nextra-theme-docs";
+import { type DocsThemeConfig, useConfig } from "nextra-theme-docs";
 
 import Logo from "./images/logo.svg";
 
 const config: DocsThemeConfig = {
+  head: () => {
+    const { frontMatter } = useConfig();
+
+    return (
+      <>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta
+          property="og:title"
+          content={`${frontMatter.title} | Nimara Storefront`}
+        />
+      </>
+    );
+  },
   logo: <Logo />,
   logoLink: "https://www.demo.nimara.store/",
   project: {
-    link: "https://github.com/mirumee/nimara-ecommerce/tree/main/apps/storefront/",
+    link: "https://github.com/mirumee/nimara-ecommerce/tree/main/apps/storefront",
   },
   docsRepositoryBase:
-    "https://github.com/mirumee/nimara-ecommerce/tree/main/apps/docs/",
+    "https://github.com/mirumee/nimara-ecommerce/tree/main/apps/docs",
   chat: {
-    link: "https://discord.com/channels/1271065464407068702/",
+    link: "https://discord.com/channels/1271065464407068702",
   },
   footer: {
-    text: `© Nimara ${new Date().getFullYear()}`,
-  },
-  useNextSeoProps() {
-    return {
-      titleTemplate: "%s – Nimara",
-    };
+    content: `© Nimara ${new Date().getFullYear()}`,
   },
 };
 
-// eslint-disable-next-line import/no-default-export
 export default config;
