ae1599a762d5d3e018418686f917f62fd78ca608
Karolka <13152501+karolkarolka@users.noreply.github.com>
2024-12-18 14:08:27 +0100

    chore: Remove unused test scenarios (#129)

--- apps/automated-tests/tests/e2e/navigation-menus/navigation-menus.spec.ts
+++
@@ -1,9 +0,0 @@
-import { test } from "@playwright/test";
-import { HomePage } from "pages/HomePage";
-
-test.skip("C10406. Breadcrumbs for Easy Navigation", async ({ page }) => {
-  const homePage = new HomePage(page);
-
-  // Navigate to home page
-  await homePage.navigate();
-});
