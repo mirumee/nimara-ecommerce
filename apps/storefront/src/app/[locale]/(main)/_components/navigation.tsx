d0cf7973452524f7b455ce2ac9ffa8f1455c609c
Wojciech Gajda <wojciech.gajda@mirumee.com>
2025-04-03 14:57:20 +0200

    fix: prevent unnecessary menu opening for non-dropdown items

--- apps/storefront/src/app/[locale]/(main)/_components/navigation.tsx
+++ apps/storefront/src/app/[locale]/(main)/_components/navigation.tsx
@@ -7,8 +7,10 @@ import {
   NavigationMenu,
   NavigationMenuContent,
   NavigationMenuItem,
+  NavigationMenuLink,
   NavigationMenuList,
   NavigationMenuTrigger,
+  navigationMenuTriggerStyle,
 } from "@nimara/ui/components/navigation-menu";
 import { RichText } from "@nimara/ui/components/rich-text";
 
@@ -36,84 +38,93 @@ export const Navigation = ({ menu }: { menu: Maybe<Menu> }) => {
             <Link
               href={item.url}
               className="text-inherit no-underline hover:underline"
+              legacyBehavior
+              passHref
             >
-              <NavigationMenuTrigger showIcon={!!item.children?.length}>
-                {item.label}
-              </NavigationMenuTrigger>
+              {!!item.children?.length ? (
+                <NavigationMenuTrigger
+                  showIcon={!!item.children?.length}
+                  onClick={() => setCurrentMenuItem("")}
+                >
+                  {item.label}
+                </NavigationMenuTrigger>
+              ) : (
+                <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                  {item.label}
+                </NavigationMenuLink>
+              )}
             </Link>
-            {!!item.children?.length && (
-              <NavigationMenuContent>
-                <div className="grid w-full grid-cols-6 p-6">
-                  <div className="col-span-2 flex flex-col gap-3 pr-6">
-                    {!!item.children?.length &&
-                      item.children
-                        ?.filter((child) => !child.collectionImageUrl)
-                        .map((child) => (
-                          <Link
-                            key={child.id}
-                            href={child.url}
-                            className="group block space-y-1 rounded-md p-3 hover:bg-accent"
-                          >
-                            <div className="text-sm font-medium leading-none">
-                              {child.label}
-                            </div>
-                            <div className="text-sm leading-snug text-muted-foreground">
-                              {child.description &&
-                              isValidJson(child.description) ? (
-                                <RichText
-                                  className="py-1"
-                                  jsonStringData={child.description}
-                                />
-                              ) : (
-                                <p className="py-1">{child.description}</p>
-                              )}
-                            </div>
-                          </Link>
-                        ))}
-                  </div>
+            <NavigationMenuContent>
+              <div className="grid w-full grid-cols-6 p-6">
+                <div className="col-span-2 flex flex-col gap-3 pr-6">
+                  {!!item.children?.length &&
+                    item.children
+                      ?.filter((child) => !child.collectionImageUrl)
+                      .map((child) => (
+                        <Link
+                          key={child.id}
+                          href={child.url}
+                          className="group block space-y-1 rounded-md p-3 hover:bg-accent"
+                        >
+                          <div className="text-sm font-medium leading-none">
+                            {child.label}
+                          </div>
+                          <div className="text-sm leading-snug text-muted-foreground">
+                            {child.description &&
+                            isValidJson(child.description) ? (
+                              <RichText
+                                className="py-1"
+                                jsonStringData={child.description}
+                              />
+                            ) : (
+                              <p className="py-1">{child.description}</p>
+                            )}
+                          </div>
+                        </Link>
+                      ))}
+                </div>
 
-                  <div className="col-span-4 grid grid-cols-3 gap-3">
-                    {!!item.children?.length &&
-                      item.children
-                        ?.filter((child) => child.collectionImageUrl)
-                        .slice(0, 3)
-                        .map((child) => (
-                          <Link
-                            key={child.id}
-                            href={child.url}
-                            className="group relative min-h-[270px] overflow-hidden rounded-lg bg-accent"
-                            onClick={() => setCurrentMenuItem("")}
-                          >
-                            <div
-                              className="h-1/2 bg-cover bg-center"
-                              style={{
-                                backgroundImage: `url(${child?.collectionImageUrl})`,
-                              }}
-                            />
-                            <div className="flex h-1/2 flex-col justify-start bg-muted/50 p-6">
-                              <div className="relative z-20 space-y-2">
-                                <div className="text-lg font-medium leading-none group-hover:underline">
-                                  {child.label}
-                                </div>
-                                <div className="text-sm leading-snug text-muted-foreground">
-                                  {child.description &&
-                                  isValidJson(child.description) ? (
-                                    <RichText
-                                      className="py-1"
-                                      jsonStringData={child.description}
-                                    />
-                                  ) : (
-                                    <p className="py-1">{child.description}</p>
-                                  )}
-                                </div>
+                <div className="col-span-4 grid grid-cols-3 gap-3">
+                  {!!item.children?.length &&
+                    item.children
+                      ?.filter((child) => child.collectionImageUrl)
+                      .slice(0, 3)
+                      .map((child) => (
+                        <Link
+                          key={child.id}
+                          href={child.url}
+                          className="group relative min-h-[270px] overflow-hidden rounded-lg bg-accent"
+                          onClick={() => setCurrentMenuItem("")}
+                        >
+                          <div
+                            className="h-1/2 bg-cover bg-center"
+                            style={{
+                              backgroundImage: `url(${child?.collectionImageUrl})`,
+                            }}
+                          />
+                          <div className="flex h-1/2 flex-col justify-start bg-muted/50 p-6">
+                            <div className="relative z-20 space-y-2">
+                              <div className="text-lg font-medium leading-none group-hover:underline">
+                                {child.label}
+                              </div>
+                              <div className="text-sm leading-snug text-muted-foreground">
+                                {child.description &&
+                                isValidJson(child.description) ? (
+                                  <RichText
+                                    className="py-1"
+                                    jsonStringData={child.description}
+                                  />
+                                ) : (
+                                  <p className="py-1">{child.description}</p>
+                                )}
                               </div>
                             </div>
-                          </Link>
-                        ))}
-                  </div>
+                          </div>
+                        </Link>
+                      ))}
                 </div>
-              </NavigationMenuContent>
-            )}
+              </div>
+            </NavigationMenuContent>
           </NavigationMenuItem>
         ))}
       </NavigationMenuList>
