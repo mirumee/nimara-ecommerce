77ee0eb1e300385996b37a72657b59c43ecc7339
Piotr Grundas <piotr.grundas@mirumee.com>
2025-04-04 09:59:18 +0200

    fix: Checkout e2e tests

--- apps/automated-tests/pages/CartPage.ts
+++ apps/automated-tests/pages/CartPage.ts
@@ -3,35 +3,23 @@ import { URLS } from "utils/constants";
 
 export class CartPage {
   readonly page: Page;
-  readonly quatityInput: Locator;
+  readonly quantityInput: Locator;
+  readonly goToCheckoutButton: Locator;
 
   constructor(page: Page) {
-    this.quatityInput = page.getByTestId("cart-product-line-qty");
     this.page = page;
+    this.quantityInput = page.getByTestId("cart-product-line-qty");
+    this.goToCheckoutButton = page.getByRole("link", {
+      name: "Go to checkout",
+    });
   }
 
-  async goto(url: string) {
-    await this.page.goto(url);
+  async goto() {
+    await this.page.goto(URLS().CART_PAGE);
   }
 
-  async gotToCheckout() {
-    await this.page.getByText("Go to checkout").click();
+  async goToCheckout() {
+    await this.goToCheckoutButton.click();
     await this.page.waitForURL(URLS().CHECKOUT_PAGE_SIGN_IN);
-  }
-
-  async isVisible() {
-    const bagPageLocator = this.page.getByText("Add to bag");
-
-    await bagPageLocator.waitFor();
-
-    return bagPageLocator.isVisible();
-  }
-
-  async verifyProductInBag(productName: string) {
-    const productLocator = this.page.locator(`text=${productName}`);
-
-    await productLocator.waitFor();
-
-    return productLocator.isVisible();
   }
 }
