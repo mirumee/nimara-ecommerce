9b56b38d3d0647a60629bc159bb83a793ba9272b
Grzegorz Derdak <grzegorz.derdak@gmail.com>
2025-04-04 12:13:03 +0200

    improve docs

--- packages/infrastructure/src/logging/service.ts
+++ packages/infrastructure/src/logging/service.ts
@@ -5,8 +5,22 @@ import { type Logger, type LogLevel } from "./types";
 
 const LOG_LEVEL: LogLevel = (process.env.LOG_LEVEL as LogLevel) || "info";
 
+/**
+ * Use this function to create a logger instance.
+ * It uses a default logger config, but you can override it by passing a config object.
+ *
+ * @see {@link Logger} for the logger type.
+ * @see {@link LogLevel} for available log levels.
+ * @see {@link LoggerOptions} for available logger options.
+ *
+ * @example
+ * import { getLogger } from "@nimara/infrastructure/logging/service";
+ * const logger = getLogger({ name: "my-app" });
+ * logger.info("Hello world");
+ * logger.error("Something went wrong");
+ */
 export const getLogger = (
-  opts?: Partial<Omit<LoggerOptions<LogLevel, true>, "level">>,
+  opts?: Partial<Omit<LoggerOptions<LogLevel>, "level">>,
 ): Logger =>
   pinoLogger({
     level: LOG_LEVEL,
@@ -14,4 +28,18 @@ export const getLogger = (
     ...opts,
   });
 
+/**
+ * Default logger instance.
+ * This instance is used by default in the application.
+ * You can override this instance by creating a new logger instance with the `getLogger` function.
+ *
+ * @see {@link Logger} for the logger type.
+ * @see {@link LogLevel} for available log levels.
+ * @see {@link LoggerOptions} for available logger options.
+ *
+ * @example
+ * import { logger } from "@nimara/infrastructure/logging/service";
+ * logger.info("Hello world");
+ * logger.error("Something went wrong");
+ */
 export const logger: Logger = getLogger();
--- packages/infrastructure/src/logging/types.ts
+++ packages/infrastructure/src/logging/types.ts
@@ -5,10 +5,37 @@
  * - warning: An indication that something unexpected happened, or indicative of some problem in the near future (e.g. ‘disk space low’). The software is still functioning as expected.
  * - error: Due to a more serious problem, the software has not been able to perform some function.
  * - critical: A serious error, indicating that the program itself may be unable to continue running.
+ *
+ * @see {@link LogFn} for the log function type.
+ * @see {@link Logger} for the logger type.
+ * @see {@link LoggerService} for the logger service type.
  */
 export type LogLevel = "debug" | "info" | "warning" | "error" | "critical";
 
+/**
+ * Log function type. It takes a message and an optional context object.
+ *
+ * @see {@link LogLevel} for available log levels.
+ * @see {@link Logger} for the logger type.
+ * @see {@link LoggerService} for the logger service type.
+ */
 export type LogFn = (message: string, context?: object) => void;
+
+/**
+ * Type of the logger instance.
+ * It contains methods for each log level.
+ * Each method takes a message and an optional context object.
+ *
+ * @see {@link LogLevel} for available log levels.
+ * @see {@link LogFn} for the log function type.
+ */
 export type Logger = Record<LogLevel, LogFn>;
 
-export type LoggerService = ({ level }: { level: LogLevel }) => Logger;
+/**
+ * Logger service type. Any function that returns a logger instance should implement this type.
+ *
+ * @see {@link Logger} for the logger type.
+ * @see {@link LogLevel} for available log levels.
+ * @see {@link LogFn} for the log function type.
+ */
+export type LoggerService = (params: { level: LogLevel }) => Logger;
