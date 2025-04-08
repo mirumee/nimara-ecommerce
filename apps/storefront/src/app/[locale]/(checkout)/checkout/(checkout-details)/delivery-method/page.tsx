3dd0026bdcddf292d9ff90b29754edec3eb3bd29
Piotr Grundas <piotrgrundas@gmail.com>
2025-04-08 09:06:21 +0200

    fix: checkout restrictions (#235)

--- apps/storefront/src/app/[locale]/(checkout)/checkout/(checkout-details)/delivery-method/page.tsx
+++ apps/storefront/src/app/[locale]/(checkout)/checkout/(checkout-details)/delivery-method/page.tsx
@@ -1,46 +1,36 @@
-import { cookies } from "next/headers";
 import { getLocale } from "next-intl/server";
 
 import { getAccessToken } from "@/auth";
-import { COOKIE_KEY } from "@/config";
-import { redirect } from "@/i18n/routing";
-import { deleteCheckoutIdCookie } from "@/lib/actions/checkout";
-import { paths } from "@/lib/paths";
-import { getCurrentRegion } from "@/regions/server";
-import { checkoutService, userService } from "@/services";
+import { getCheckoutOrRedirect } from "@/lib/checkout";
+import { userService } from "@/services";
 
 import { CheckoutSkeleton } from "../../_components/checkout-skeleton";
 import { EmailSection } from "../../_sections/email-section";
 import { PaymentSection } from "../../_sections/payment-section";
 import { ShippingAddressSection } from "../../_sections/shipping-address-section";
+import { validateCheckoutStepAction } from "../../actions";
 import { DeliveryMethodForm } from "./form";
 
 export default async function Page() {
-  const checkoutId = (await cookies()).get(COOKIE_KEY.checkoutId)?.value;
-  const accessToken = await getAccessToken();
-  const locale = await getLocale();
-  const region = await getCurrentRegion();
-
-  if (!checkoutId) {
-    redirect({ href: paths.cart.asPath(), locale });
-  }
-
-  const { checkout } = await checkoutService.checkoutGet({
-    checkoutId,
-    languageCode: region.language.code,
-    countryCode: region.market.countryCode,
-  });
-
-  if (!checkout) {
-    await deleteCheckoutIdCookie();
-    redirect({ href: paths.cart.asPath(), locale });
-  }
+  const checkout = await getCheckoutOrRedirect();
 
   if (checkout.problems.insufficientStock.length) {
     return <CheckoutSkeleton />;
   }
 
-  const user = await userService.userGet(accessToken);
+  const accessToken = await getAccessToken();
+
+  const [user, locale] = await Promise.all([
+    userService.userGet(accessToken),
+    getLocale(),
+  ]);
+
+  await validateCheckoutStepAction({
+    checkout,
+    user,
+    locale,
+    step: "delivery-method",
+  });
 
   return (
     <>
--- apps/storefront/src/app/[locale]/(checkout)/checkout/page.tsx
+++ apps/storefront/src/app/[locale]/(checkout)/checkout/page.tsx
@@ -1,50 +1,25 @@
-import { cookies } from "next/headers";
 import { getLocale } from "next-intl/server";
 
 import { getAccessToken } from "@/auth";
-import { COOKIE_KEY } from "@/config";
-import { redirect } from "@/i18n/routing";
-import { deleteCheckoutIdCookie } from "@/lib/actions/checkout";
-import { paths } from "@/lib/paths";
-import { getCurrentRegion } from "@/regions/server";
-import { checkoutService, userService } from "@/services";
+import { getCheckoutOrRedirect } from "@/lib/checkout";
+import { userService } from "@/services";
+
+import { CheckoutSkeleton } from "./_components/checkout-skeleton";
+import { validateCheckoutStepAction } from "./actions";
 
 export default async function Page() {
-  const checkoutId = (await cookies()).get(COOKIE_KEY.checkoutId)?.value;
-  const accessToken = await getAccessToken();
-  const locale = await getLocale();
+  const checkout = await getCheckoutOrRedirect();
 
-  const [region, user] = await Promise.all([
-    getCurrentRegion(),
+  if (checkout?.problems.insufficientStock.length) {
+    return <CheckoutSkeleton />;
+  }
+
+  const accessToken = await getAccessToken();
+
+  const [user, locale] = await Promise.all([
     userService.userGet(accessToken),
+    getLocale(),
   ]);
 
-  if (!checkoutId) {
-    redirect({ href: paths.cart.asPath(), locale });
-  }
-
-  const { checkout } = await checkoutService.checkoutGet({
-    checkoutId,
-    languageCode: region.language.code,
-    countryCode: region.market.countryCode,
-  });
-
-  if (!checkout) {
-    await deleteCheckoutIdCookie();
-    redirect({ href: paths.cart.asPath(), locale });
-  }
-
-  if (!checkout.email && !user?.email) {
-    redirect({ href: paths.checkout.userDetails.asPath(), locale });
-  }
-
-  if (user?.email || !checkout.shippingAddress) {
-    redirect({ href: paths.checkout.shippingAddress.asPath(), locale });
-  }
-
-  if (!checkout.deliveryMethod) {
-    redirect({ href: paths.checkout.deliveryMethod.asPath(), locale });
-  }
-
-  redirect({ href: paths.checkout.payment.asPath(), locale });
+  await validateCheckoutStepAction({ user, locale, checkout, step: null });
 }
