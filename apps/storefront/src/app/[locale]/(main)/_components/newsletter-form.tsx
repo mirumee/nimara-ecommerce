88f6ff9eec0d9554f63744bf74fe7d6fa5f3f689
Wojciech Gajda <wojciech.gajda@mirumee.com>
2025-04-17 10:53:50 +0200

    feat(newsletter): prepare a newsletter component MS-955 (#257)

---
+++ apps/storefront/src/app/[locale]/(main)/_components/newsletter-form.tsx
@@ -0,0 +1,91 @@
+"use client";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Mail } from "lucide-react";
+import { useTranslations } from "next-intl";
+import { type SubmitHandler, useForm } from "react-hook-form";
+
+import { Button } from "@nimara/ui/components/button";
+import { Form } from "@nimara/ui/components/form";
+import { useToast } from "@nimara/ui/hooks";
+
+import { TextFormField } from "@/components/form/text-form-field";
+
+import { newsletterSubscribeAction } from "./action";
+import { type FormSchema, formSchema } from "./schema";
+
+export const Newsletter = () => {
+  const t = useTranslations();
+  const { toast } = useToast();
+
+  const form = useForm<FormSchema>({
+    resolver: zodResolver(formSchema({ t })),
+    defaultValues: {
+      name: "",
+      email: "",
+    },
+  });
+
+  const isPending = form.formState.isSubmitting;
+
+  const handleSubmit: SubmitHandler<FormSchema> = async (values) => {
+    const result = await newsletterSubscribeAction(values);
+
+    if (result.ok) {
+      form.reset();
+
+      toast({
+        description: t("newsletter.subscribe-success"),
+        position: "center",
+      });
+    } else {
+      toast({
+        description: t("newsletter.subscribe-error"),
+        position: "center",
+        variant: "destructive",
+      });
+    }
+  };
+
+  return (
+    <section className="rounded-md bg-muted px-4 py-12 dark:bg-gray-800 sm:px-6 lg:px-8">
+      <div className="mx-auto max-w-3xl">
+        <div className="text-center">
+          <Mail className="mx-auto h-12 w-12 text-gray-400" />
+          <h2 className="mt-2 text-3xl font-extrabold text-gray-900 dark:text-muted sm:text-4xl">
+            {t("newsletter.subscribe-title")}
+          </h2>
+          <p className="mt-4 text-lg text-gray-600 dark:text-gray-300">
+            {t("newsletter.subscribe-description")}
+          </p>
+        </div>
+        <Form {...form}>
+          <form
+            onSubmit={form.handleSubmit(handleSubmit)}
+            className="mt-8 items-end gap-4 sm:flex"
+            noValidate
+          >
+            <TextFormField
+              name="name"
+              label={t("newsletter.subscribe-name-field-label")}
+              placeholder={t("newsletter.subscribe-name-field-placeholder")}
+              type="text"
+              disabled={isPending}
+            />
+            <TextFormField
+              name="email"
+              label={t("newsletter.subscribe-email-field-label")}
+              placeholder={t("newsletter.subscribe-email-field-placeholder")}
+              type="email"
+              disabled={isPending}
+            />
+            <div className="mt-3 rounded-md sm:mt-0 sm:flex-shrink-0">
+              <Button type="submit" disabled={isPending} loading={isPending}>
+                {t("newsletter.subscribe-cta")}
+              </Button>
+            </div>
+          </form>
+        </Form>
+      </div>
+    </section>
+  );
+};
---
+++ apps/storefront/src/app/[locale]/(main)/_components/schema.ts
@@ -0,0 +1,15 @@
+import * as z from "zod";
+
+import { type GetTranslations } from "@/types";
+
+export const formSchema = ({ t }: { t: GetTranslations }) =>
+  z.object({
+    name: z.string().min(1, { message: t("form-validation.required") }),
+    email: z
+      .string()
+      .min(1, { message: t("form-validation.email-required") })
+      .email({ message: t("form-validation.invalid-email") })
+      .trim(),
+  });
+
+export type FormSchema = z.infer<ReturnType<typeof formSchema>>;
