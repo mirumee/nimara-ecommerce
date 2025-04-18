15aef195014b7b60fbcb9ca2fdcfd039bbba3a4e
soniaklimas <57659625+soniaklimas@users.noreply.github.com>
2025-04-18 12:19:59 +0200

    perf: Improve SEO on PDP and collection page (#258)

--- apps/storefront/src/components/header/logo.tsx
+++ apps/storefront/src/components/header/logo.tsx
@@ -3,21 +3,26 @@
 import { useTranslations } from "next-intl";
 
 import BrandLogo from "@/assets/brand-logo-dark.svg";
-import { Link } from "@/i18n/routing";
+import { Link, usePathname } from "@/i18n/routing";
 import { paths } from "@/lib/paths";
 
 export const Logo = () => {
   const t = useTranslations("common");
+  const pathname = usePathname();
+  const asH1 =
+    !pathname.includes("products") && !pathname.includes("collection");
+  const Wrapper = asH1 ? "h1" : "span";
 
   return (
-    <h1
-      className="flex justify-center align-middle md:justify-start"
-      aria-label={t("logo")}
-    >
+    <Wrapper className="flex justify-center md:justify-start">
       <Link href={paths.home.asPath()} title={t("go-to-homepage")}>
-        <BrandLogo height={36} className="fill-primary" />
+        <BrandLogo
+          height={36}
+          className="fill-primary"
+          aria-label={t("logo")}
+        />
       </Link>
-    </h1>
+    </Wrapper>
   );
 };
 
