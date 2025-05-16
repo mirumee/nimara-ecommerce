cada1307a76dcc6cd3790c479925f66a1016ad8d
Wojciech Gajda <wojciech.gajda@mirumee.com>
2025-05-16 09:33:35 +0200

    fix(checkout): remove redirect from checkout remover MS-1016 (#298)
    I want to merge this change because...
    
    <!-- Please mention all relevant issue numbers. -->
    
    # Pull Request Checklist
    
    <!-- Please keep this section. It will make the maintainer's life
    easier. -->
    
    - [ ] Test the changes locally to ensure they work as expected.
    - [ ] Document the testing process and results in the pull request
    description. (Screen recording, screenshot etc)
    - [ ] Include new tests for any new functionality or significant
    changes.
    - [ ] Ensure that tests cover edge cases and potential failure points.
    - [ ] Document the impact of the changes on the system, including
    potential risks and benefits.
    - [ ] Provide a rollback plan in case the changes introduce critical
    issues.
    - [ ] Update documentation to reflect any changes in functionality.

--- apps/storefront/src/app/[locale]/order/confirmation/[id]/actions.ts
+++ apps/storefront/src/app/[locale]/order/confirmation/[id]/actions.ts
@@ -1,14 +1,11 @@
 "use server";
 
 import { cookies } from "next/headers";
-import { redirect, RedirectType } from "next/navigation";
 
 import { COOKIE_KEY } from "@/config";
-import { paths } from "@/lib/paths";
 
-export const clearCheckoutCookieAction = async ({ id }: { id: string }) => {
+export const clearCheckoutCookieAction = async () => {
   const nextCookies = await cookies();
 
   nextCookies.delete(COOKIE_KEY.checkoutId);
-  redirect(paths.order.confirmation.asPath({ id }), RedirectType.replace);
 };
--- apps/storefront/src/app/[locale]/order/confirmation/[id]/components/checkout-remover.tsx
+++ apps/storefront/src/app/[locale]/order/confirmation/[id]/components/checkout-remover.tsx
@@ -2,7 +2,8 @@
 
 import { useEffect } from "react";
 
-import { QUERY_PARAMS } from "@/lib/paths";
+import { useRouter } from "@/i18n/routing";
+import { paths, QUERY_PARAMS } from "@/lib/paths";
 
 import { clearCheckoutCookieAction } from "../actions";
 
@@ -13,12 +14,18 @@ export const CheckoutRemover = ({
   params: Promise<{ id: string }>;
   searchParams: Promise<{ [QUERY_PARAMS.orderPlaced]: string }>;
 }) => {
+  const router = useRouter();
+
   useEffect(() => {
     void (async () => {
       if (QUERY_PARAMS.orderPlaced in (await searchParams)) {
         const { id } = await params;
 
-        await clearCheckoutCookieAction({ id });
+        await clearCheckoutCookieAction();
+
+        // After clearing the checkout cookie, we remove the `?orderPlaced=true` query param
+        // to prevent the action from being triggered again on page reload.
+        router.replace(paths.order.confirmation.asPath({ id }));
       }
     })();
   }, []);
