42362d47bab67b523a8a4207c36f886e9abe8fcc
KJmirumee <klaudia.jaroszewicz@mirumee.com>
2025-06-05 10:25:33 +0200

    style(BasePage.ts): added missing space in import
    white space was missing between Locator and type in first import line

--- apps/automated-tests/pages/BasePage.ts
+++ apps/automated-tests/pages/BasePage.ts
@@ -1,4 +1,4 @@
-import { expect, type Locator,type Page } from "@playwright/test";
+import { expect, type Locator, type Page } from "@playwright/test";
 import { user } from "utils/constants";
 
 export class BasePage {
