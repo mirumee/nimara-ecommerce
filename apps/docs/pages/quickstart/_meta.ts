9e2d61672506ade344c916ce429cc31890013e03
soniaklimas <57659625+soniaklimas@users.noreply.github.com>
2025-06-25 09:06:37 +0200

    docs: Add env variables and Stripe guides (#382)

--- apps/docs/pages/quickstart/_meta.ts
+++ apps/docs/pages/quickstart/_meta.ts
@@ -1,5 +1,7 @@
 export default {
   "running-locally": "Running locally",
   storefront: "Storefront",
+  "environment-variables": "Environment variables",
+  "stripe-integration": "Stripe integration",
   "add-content-with-saleor-cms": "Saleor CMS",
 };
