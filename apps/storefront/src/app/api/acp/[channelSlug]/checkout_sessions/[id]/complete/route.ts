ec05d7dfeb4e96e8d01e6d497d4ff235277a4a4a
Damian Wysocki <damian.wysocki@mirumee.com>
2025-10-10 09:59:35 +0200

    add checkout complete

--- apps/storefront/src/app/api/acp/[channelSlug]/checkout_sessions/[id]/complete/route.ts
+++ apps/storefront/src/app/api/acp/[channelSlug]/checkout_sessions/[id]/complete/route.ts
@@ -1,11 +1,64 @@
 import { revalidateTag } from "next/cache";
 import { type NextRequest, NextResponse } from "next/server";
+import { getPaymentService } from "@/services/payment";
+import { saleorAcPService } from "@nimara/infrastructure/mcp/saleor/service";
+import { clientEnvs } from "@/envs/client";
+import { storefrontLogger } from "@/services/logging";
 
 export async function POST(
   _request: NextRequest,
   { params }: { params: Promise<{ channelSlug: string; id: string }> },
 ) {
-  const { id } = await params;
+  const { channelSlug, id } = await params;
+
+  const acpService = saleorAcPService({
+    apiUrl: clientEnvs.NEXT_PUBLIC_SALEOR_API_URL,
+    logger: storefrontLogger,
+    channel: channelSlug,
+    storefrontUrl: clientEnvs.NEXT_PUBLIC_STOREFRONT_URL,
+  });
+
+  // get checkout session by id and channelSlug from request params
+  const paymentService = await getPaymentService();
+  // secretTToken from payment gateway
+  const checkout_session = await acpService.getCheckoutSession({
+    checkoutSessionId: id,
+  });
+  const checkout = checkout_session.data?.checkoutSession;
+
+  if (!checkout) {
+    storefrontLogger.error(
+      "Failed to fetch checkout session for channel ${channelSlug} and id ${id}.",
+    );
+
+    return NextResponse.json(
+      { status: "Error fetching checkout session" },
+      { status: 500 },
+    );
+  }
+
+  const transaction = await paymentService.paymentGatewayTransactionInitialize({
+    id: checkout.id,
+    amount: checkout.totalPrice.gross.amount,
+    sharedPaymentToken: checkout.sharedPaymentToken, // TODO
+  });
+
+  if (!transaction) {
+    storefrontLogger.error(
+      "Failed to initialize payment transaction for checkout session ${id}.",
+    );
+    return NextResponse.json(
+      { status: "Error initializing payment transaction" },
+      { status: 500 },
+    );
+  }
+
+  const transactionId = transaction.data?.transactionId;
+  const result = await paymentService.paymentResultProcess({
+    checkout: {},
+    searchParams: { transactionId: transactionId! },
+  });
+  // TODO tutaj zrobic chcekout comple
 
   // TODO: Add logic here to complete the checkout session with the given ID
 
--- packages/infrastructure/src/mcp/saleor/infrastructure/checkout-session-complete-infra.ts
+++ packages/infrastructure/src/mcp/saleor/infrastructure/checkout-session-complete-infra.ts
@@ -1,15 +1,110 @@
 import { GraphqlClient } from "#root/graphql/client";
 import { Logger } from "#root/logging/types";
-import { ok } from "@nimara/domain/objects/Result";
+import { ok, err } from "@nimara/domain/objects/Result";
+import { CheckoutSessionGetDocument } from "#root/mcp/saleor/graphql/queries/generated";
+import { StripePaymentService } from "#root/payment/providers";
+
+const DEFAULT_CACHE_TIME = 60 * 60; // 1 hour
 
 export const checkoutSessionCompleteInfra = async ({
+  deps,
   input,
 }: {
   input: { checkoutSessionId: string };
   deps: {
+    paymentService: StripePaymentService;
     graphqlClient: GraphqlClient;
     logger: Logger;
   };
 }) => {
+  const result = await deps.graphqlClient.execute(CheckoutSessionGetDocument, {
+    variables: {
+      id: input.checkoutSessionId,
+    },
+    options: {
+      next: {
+        revalidate: DEFAULT_CACHE_TIME,
+        tags: [`MCP_CHECKOUT_SESSION:${input.checkoutSessionId}`],
+      },
+    },
+    operationName: "ACP:CheckoutSessionQuery",
+  });
+
+  if (!result.ok) {
+    deps.logger.error("Failed to fetch checkout session from Saleor", {
+      errors: result.errors,
+    });
+
+    return err([
+      {
+        code: "BAD_REQUEST_ERROR",
+      },
+    ]);
+  }
+
+  if (!result.data.checkout) {
+    deps.logger.error("No checkout found in Saleor", {
+      checkoutId: input.checkoutSessionId,
+    });
+
+    return err([
+      {
+        code: "BAD_REQUEST_ERROR",
+      },
+    ]);
+  }
+
+  const transaction =
+    await deps.paymentService.paymentGatewayTransactionInitialize({
+      id: result.data.checkout.id,
+      amount: result.data.checkout.totalPrice.gross.amount,
+      sharedPaymentToken: result.data.checkout.sharedPaymentToken, // TODO
+    });
+
+  if (!transaction) {
+    deps.logger.error(
+      "Failed to initialize payment transaction for checkout session ${input.checkoutSessionId}.",
+    );
+    return err([
+      {
+        code: "BAD_REQUEST_ERROR",
+      },
+    ]);
+  }
+
+  const transactionId = transaction.data?.transactionId;
+
+  const paymentResult = await deps.paymentService.paymentResultProcess({
+    checkout: result.data.checkout,
+    searchParams: { transactionId: transactionId! },
+  });
+
+  if (!paymentResult.ok) {
+    deps.logger.error(
+      "Failed to process payment result for checkout session ${input.checkoutSessionId}.",
+      { errors: paymentResult.errors },
+    );
+    return err([
+      {
+        code: "BAD_REQUEST_ERROR",
+      },
+    ]);
+  }
+
+  if (!paymentResult.data.success) {
+    deps.logger.error(
+      "Payment not completed for checkout session ${input.checkoutSessionId}.",
+    );
+    return err([
+      {
+        code: "PAYMENT_NOT_COMPLETED_ERROR",
+      },
+    ]);
+  }
+
+  deps.graphqlClient.execute();
+
+  // Here you can add logic to complete the checkout in Saleor if needed
+
   return ok({ orderId: input.checkoutSessionId });
 };
