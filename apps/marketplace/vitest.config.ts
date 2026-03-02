ad3598dfdae2d34551e67132be0306c8fbfb79b7
Adam Bogdał <adam@bogdal.pl>
2026-03-02 18:13:16 +0100

    feat: Add alias to vitest

--- apps/marketplace/vitest.config.ts
+++ apps/marketplace/vitest.config.ts
@@ -1,6 +1,12 @@
 import { defineConfig } from "vitest/config";
+import { fileURLToPath } from "node:url";
 
 export default defineConfig({
+  resolve: {
+    alias: {
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
   test: {
     passWithNoTests: true,
   },
