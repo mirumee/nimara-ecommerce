e10d97af966d57e0a4e1846469b043cd5183ecef
Witek <witold.turowicz@mirumee.com>
2026-08-11 13:51:18 +0200

    Comments PL -> EN

--- apps/automated-tests/codecept/data/locales.ts
+++ apps/automated-tests/codecept/data/locales.ts
@@ -3,9 +3,9 @@ const LOCALE_DICTIONARY: Record<string, string> = {
   gb: "/gb",
 };
 
-// Pobranie zmiennej środowiskowej LOCALE (domyślnie np. 'us')
+// LOCALE - default to "us" if not provided
 export const REQUESTED_LOCALE = process.env.LOCALE?.toLowerCase() || "us";
 
-// Wyciągnięcie odpowiedniego przedrostka URL
+// Sets prefix for URLs used in tests
 export const LOCALE_PREFIX =
   LOCALE_DICTIONARY[REQUESTED_LOCALE] || LOCALE_DICTIONARY["us"];
