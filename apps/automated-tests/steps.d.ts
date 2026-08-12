f8cc5d1cf196b63bda3fcc772ae464ea6f944118
Witek <witold.turowicz@mirumee.com>
2026-08-12 14:10:16 +0200

    Autogenerated by "npx codeceptjs def ran in /apps/automated-tests

--- apps/automated-tests/steps.d.ts
+++ apps/automated-tests/steps.d.ts
@@ -1,15 +1,19 @@
 /// <reference types='codeceptjs' />
 type homepage = typeof import("./codecept/pages/homepage").default;
+type homepagePage = typeof import("./codecept/pages/homepagePage").default;
+type productPage = typeof import("./codecept/pages/productPage").default;
+type checkoutPage = typeof import("./codecept/pages/checkoutPage").default;
+type bagPage = typeof import("./codecept/pages/cartPage").default;
 
 declare namespace CodeceptJS {
   interface SupportObject {
     I: I;
-    bagPage: any;
-    checkoutPage: any;
+    bagPage: bagPage;
+    checkoutPage: checkoutPage;
     current: any;
     homepage: homepage;
-    homepagePage: any;
-    productPage: any;
+    homepagePage: homepagePage;
+    productPage: productPage;
   }
   interface Methods extends Playwright {}
   interface I extends WithTranslation<Methods> {}
