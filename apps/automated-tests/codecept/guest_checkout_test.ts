e7bc6405d46566fdf64376814d6b00ca5e1ff2cc
Witek <witold.turowicz@mirumee.com>
2026-08-12 14:29:45 +0200

    redundant argument removed

--- apps/automated-tests/codecept/guest_checkout_test.ts
+++ apps/automated-tests/codecept/guest_checkout_test.ts
@@ -16,7 +16,7 @@ Scenario(
     cartPage.go_to_checkout_from_bag(timeout_seconds);
     checkoutPage.continue_as_guest(timeout_seconds);
     checkoutPage.guest_step1_email(timeout_seconds);
-    await checkoutPage.guest_step2_continue_as_guest(timeout_seconds);
+    await checkoutPage.guest_step2_continue_as_guest();
     checkoutPage.guest_step3_shipping_address(timeout_seconds);
     checkoutPage.guest_step4_shipping_method_dhl_normal(timeout_seconds);
     checkoutPage.guest_step5_payment_details("valid", timeout_seconds);
