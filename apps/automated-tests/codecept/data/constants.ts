d0ba41b7bcbe9c70a3c19a138a8792b0864d84e5
Witek <witold.turowicz@mirumee.com>
2026-08-13 16:58:23 +0200

    test: adding new test scenarios: checkout with 2 items in cart , checkout with proceeding to the cart via menu , negative scenarios - stolen card, expired card

--- apps/automated-tests/codecept/data/constants.ts
+++ apps/automated-tests/codecept/data/constants.ts
@@ -21,10 +21,15 @@ export const URLS = {
   PRODUCT_PAGE: `${LOCALE_PREFIX}/products`,
   CATEGORY_PAGE: `${LOCALE_PREFIX}/categories`,
   TSHIRT_PRODUCT_PAGE: `${LOCALE_PREFIX}/products/abstract-tshirt-black`,
+  BLACK_SAND_PRODUCT_PAGE: `${LOCALE_PREFIX}/products/black-sand`,
 } as const;
 
 export const tshirtImagelocator = locate("img").withAttr({
   "aria-label": "Abstract Tshirt Ultra Black",
+});
+
+export const black_sandImagelocator = locate("img").withAttr({
+  "aria-label": "Black sand",
 });
 
 export const storeHeaders = {
@@ -37,6 +42,7 @@ export const storeHeaders = {
   cookiePopup: "We use cookies",
   cookieAccept: "Accept all",
   orderSuccess: "Your order has been successfully placed",
+  cardDeclined: "There was an error with your card",
 };
 
 export const userGB = {
@@ -72,8 +78,8 @@ export const card = {
   number: {
     valid: "4242 4242 4242 4242",
     invalid: "1234 5678 9012 3456",
-    stolen: "4000 0000 0000 0002",
-    declined: "4000 0000 0000 9995",
+    stolen: "4000 0000 0000 9979",
+    declined: "4000 0000 0000 9995", //generic decline
     expired: "4000 0000 0000 0069",
   },
   expiration: `12/${(new Date().getFullYear() + 1).toString().substring(2)}`,
--- apps/automated-tests/codecept/pages/homepagePage.ts
+++ apps/automated-tests/codecept/pages/homepagePage.ts
@@ -1,4 +1,9 @@
-import { storeHeaders, tshirtImagelocator, URLS } from "../data/constants";
+import {
+  black_sandImagelocator,
+  storeHeaders,
+  tshirtImagelocator,
+  URLS,
+} from "../data/constants";
 
 const { I } = inject();
 
@@ -10,9 +15,14 @@ export default {
     I.click(storeHeaders.cookieAccept);
     I.dontSee(storeHeaders.cookiePopup);
   },
-  click_on_product(timeout: number) {
+  click_on_product1(timeout: number) {
     I.scrollTo(tshirtImagelocator);
     I.click(tshirtImagelocator);
     I.waitUrlEquals(URLS.TSHIRT_PRODUCT_PAGE, timeout);
+  },
+  click_on_product2(timeout: number) {
+    I.scrollTo(black_sandImagelocator);
+    I.click(black_sandImagelocator);
+    I.waitInUrl(URLS.BLACK_SAND_PRODUCT_PAGE, timeout);
   },
 };
